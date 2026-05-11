434ab9ea05b782d039304ae4262dc92e360ec9b4
dragonoverlord3000 <hugomn2002@gmail.com>
2026-05-11 08:02:00 +0200

    more bernoulli matrices

--- benchmarks/bernoulli_random_download_helper.py
+++ benchmarks/bernoulli_random_download_helper.py
@@ -1,9 +1,7 @@
 import itertools, random, os
 random.seed(628)
-for n, betax10 in itertools.product(range(15, 21), range(1, 10)):
-    M = [[0] * n for _ in range(n)]
-    for i, j in itertools.product(range(n), range(n)):
-        M[i][j] = int(10 * random.random() < betax10)
+for n, betax40 in itertools.product(range(12, 29), range(1, 40)):
+    M = [[int(40 * random.random() < betax40) for j in range(n)] for i in range(n)]
     os.makedirs("./benchmarks/bernoulli/dataset", exist_ok=True)
-    with open(f"./benchmarks/bernoulli/dataset/n_{n}_bx10_{betax10}.txt", "w") as f:
+    with open(f"./benchmarks/bernoulli/dataset/n_{n}_bx40_{betax40}.txt", "w") as f:
         f.write(f"{n} {n}\n" + "\n".join([" ".join([str(v) for v in row]) for row in M]))
